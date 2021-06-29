e596ad442520d423baecb22d5c6d1043c722cdd2
joeljdv <joeldiazjdv@gmail.com>
2021-06-29 17:13:02 -0400

    commit tree 8409828b3c44081d13e12d20ee68e45caf9f12d1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    
+    if params[:user_id]
+      use = User.find_by(id: params[:user_id])
+      user_items = use.items
+      render json: user_items, include: :user
+    else
+      user_items = Item.all 
+      render json: user_items, include: :user
+    end  
+  end
+
+  def show
+  item = Item.find_by(id: params[:id])
+    if item
+      render json: item, include: :user
+    else
+      render json: {error: "No such Item"}, status: :not_found
+    end
+  end
+
+  def create
+    user = User.find_by(id: params[:user_id])
+    if user
+      item = user.items.create(params.permit(:name, :description, :price))
+      render json: item, status: :created
+    else
+      render json: {error: "User not found"}, status: :not_found
+    end
   end
 
 end
